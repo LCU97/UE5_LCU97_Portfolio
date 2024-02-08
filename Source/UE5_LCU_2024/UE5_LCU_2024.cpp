a6240daa6d6ab74200e0134495a4566eb9de333a
LCU <saparationlee@gmail.com>
2024-02-08 14:12:07 +0900

    02.28 시작

---
+++ Source/UE5_LCU_2024/UE5_LCU_2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_LCU_2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_LCU_2024, "UE5_LCU_2024" );
---
+++ Source/UE5_LCU_2024/UE5_LCU_2024GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_LCU_2024GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_LCU_2024_API AUE5_LCU_2024GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
